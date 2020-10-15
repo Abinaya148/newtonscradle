bff9e0a7516bbf62c2ef6881e599e14c7843ff65
Abinaya148 <70847705+Abinaya148@users.noreply.github.com>
2020-10-15 09:51:03 +0530

    Add files via upload

---
+++ ball2.js
@@ -0,0 +1,29 @@
+class Ball2{
+    constructor(x,y)
+    {
+        var options={
+            restitution:1.0,
+            friction:0.8,
+            density:1.0,
+            isStatic:true
+        }
+        this.radius=40;
+        this.body=Bodies.circle(x,y,40,options);
+        World.add(world,this.body);
+
+    }
+    display()
+    {
+        this.body.position.x = mouseX;
+        this.body.position.y = mouseY;
+        var pos=this.body.position;
+        var angle=this.body.angle;
+        push();
+        translate(pos.x,pos.y);
+        rotate(angle)
+        strokeWeight(2)
+        fill("pink")
+        circle(0,0,this.radius);
+        pop();
+    }
+}
